4f96f416857a45d00288fe472ef2115a90410e3b
Robert Mwangi <robertmwangi@Robert-MBP.local>
2025-05-21 08:22:27 -0700

    Exercise 1.8 & 1.9 complete.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -203,10 +203,30 @@ const Button = (props) => {
   )
 }
 
-
-
-
-
+// Statistics Component
+const Statistics = (props) => {
+  console.log(props.all)
+  if (props.all === 0)
+  {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>Good {props.good}</p>
+      <p>Neutral {props.neutral}</p>
+      <p>Bad {props.bad}</p>
+      <p>All {props.all}</p>
+      <p>Average {props.average} </p>
+      <p>Positive {props.positive} %</p>
+    </div>
+    
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -283,13 +303,9 @@ const handleBadClick = () => {
      <Button onClick={handleGoodClick} text="Good" />
      <Button onClick={handleNeutralClick} text="Neutral" />
      <Button onClick={handleBadClick} text="Bad" />
-     <Header text="Statistics" />
-     <p>Good {good}</p>
-     <p>Neutral {neutral}</p>
-     <p>Bad {bad}</p>
-     <p>All {total}</p>
-     <p>Average {calculateAverage()} </p>
-     <p>Positive {calculatePositive()} %</p>
+     
+     <Statistics good={good} neutral={neutral} bad={bad} all={total}
+                 average={calculateAverage()} positive={calculatePositive()} />
     </div>
   )
 }
