857f13a760ee6ba45c3ae30ee3328b34c4a19891
Robert Mwangi <robertmwangi@Robert-MBP.local>
2025-05-23 07:57:03 -0700

    Exercises 2.1 - 2.5 complete.

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -14,56 +14,113 @@ import Course from './components/Course'
 //     </div>
 //   )
 // }
+
+
+
+
+
 const App = () => {
-  const course = 
-  {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-
-  }
-
-  const total = course.parts.reduce(
-    (accumulator, part) => {return accumulator += part.exercises},0
-  )
-
-  console.log(total)
-
   
+  const courses = 
+  [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  // const total = courses.parts.reduce(
+  //   (accumulator, part, i) => {return accumulator += courses[i].parts[i].exercises},0
+  // )
+
+  // console.log(total)
+
+  // const total = parts.reduce((acc, part, i) => {return acc += part[i].exercises}, 0)
+  // console.log(total)
+
+  // const result = courses.map(course => course.name)
+  // console.log(result)
+
+  // const result = courses.map(course => course.name)
+  // console.log(result)
+  
+  // const result2 = courses.map(course => 
+  //   courses.map(course => 
+  //     course.parts.map(part => 
+  //       part.name
+  //     )
+  //   )
+  // )
+  // console.log(result2)
 
   return (
     <div>
-      <h2>{course.name}</h2>
-      <ul>
-        {course.parts.map(part => 
-          <Course key={part.id} course={part.name} exercises={part.exercises} />
+      {courses.map(course =>
+        <Course key={course.id} course={course} /> 
+      )}
+      
+    </div>
+   
+      /* {courses.map(course => (
+        <div key={course.id}>
+          <Header course={course} />
+          {courses.map(course => 
+            course.parts.map(part => 
+              <Content key={part.id} part={part} />
+            )
+          )} */
+        
+      
+      
+      /* <h2>{courses.name}</h2> */
+      /* <Header key={course.id} courses={courses.name}  /> */
+      /* <p>
+        {courses.parts.map(part => 
+          <Course key={part.id} courses={part.name} exercises={part.exercises} />
       )}
       <p>total of {total} exercises</p>
-      </ul>
-    </div>
+      </p> */
     
-  ) 
+)
+
 }
 
 
--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,10 +1,49 @@
 const Course = (props) => {
-    const { course, exercises } = props
-    // console.log(course)
-    // console.log(exercises)
+    console.log(props)
+    const { course } = props
+    console.log(course.parts)
+    
+    const total = course.parts.reduce((acc, part) => {
+                        return acc += part.exercises
+    },0)
+    console.log(total)
     return (
-        <li>{course} {exercises}</li>
+        <>
+        <Header course={course} />
+        {course.parts.map(part => 
+            <div key={part.id}>
+                <Content part={part} />
+            </div>
+        )}
+        <h3>total of {total} exercises</h3>
+        </>
+        
+            // console.log(course)
+            // <Header course={course} />
+            // console.log(part)
+            // <Content part={part} />
+        
     ) 
 }
+
+// Header component
+const Header = ({course}) => {
+//   console.log(course)
+  return (
+    <h2>{course.name}</h2>
+    
+  )
+}
+
+const Content = (props) => {
+    // console.log(props)
+    const { part } = props
+    // console.log(part)
+    return (
+        <p>{part.name} {part.exercises}</p>
+    )
+}
+
+
 
 export default Course
